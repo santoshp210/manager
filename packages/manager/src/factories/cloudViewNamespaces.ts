229978adfc3f0337a0f3e3bc0dd236fba0c3277d
Santosh Preetham S <santos@akamai.com>
2024-03-21 10:18:33 +0530

    upcoming: [DI-16649] - Completed Pagination and ordering of the namespace list and enabled per namespace call for namespace api key

--- packages/manager/src/factories/cloudViewNamespaces.ts
+++ packages/manager/src/factories/cloudViewNamespaces.ts
@@ -1,18 +1,29 @@
-import { Namespace } from '@linode/api-v4/lib/cloudview/types';
+import { Namespace, NamespaceApiKey } from '@linode/api-v4/lib/cloudview/types';
 import * as Factory from 'factory.ts';
 
+import { pickRandom } from 'src/utilities/random';
+
+const timestamps = [
+  '2023-07-12T16:08:53',
+  '2023-07-12T04:30:53',
+  '2023-09-15T10:42:53',
+  '2023-09-15T00:00:00',
+];
+
+const regions = ['us-east', 'us-west', 'jp-osa', 'ap-west'];
 export const namespaceFactory = Factory.Sync.makeFactory<Namespace>({
+  // created: '2023-07-12T16:08:53',
+  created: Factory.each(() => pickRandom(timestamps)),
   id: Factory.each((i) => i),
   label: Factory.each((i) => `cloudview-${i}`),
-  region: 'us-east',
+  region: Factory.each(() => pickRandom(regions)),
   type: 'metric',
+  // eslint-disable-next-line perfectionist/sort-objects
+  updated: '2023-07-12T16:08:55',
   urls: {
     ingest: 'https://cloudviewtestingest.com',
     read: 'https://cloudviewtestread.com',
     // eslint-disable-next-line perfectionist/sort-objects
     agent_install: 'https://cloudviewtestinstall.com',
   },
-  // eslint-disable-next-line perfectionist/sort-objects
-  created: '2023-07-12T16:08:53',
-  updated: '2023-07-12T16:08:55',
 });
---
+++ packages/manager/src/features/CloudView/Namespaces/NamespaceList/NamespaceList.test.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { QueryClient } from 'react-query';
+
+import { namespaceFactory } from 'src/factories';
+import { formatDate } from 'src/utilities/formatDate';
+import { mockMatchMedia, renderWithTheme } from 'src/utilities/testHelpers';
+
+import { NamespaceList } from './NamespaceList';
+
+const queryClient = new QueryClient();
+beforeAll(() => mockMatchMedia());
+afterEach(() => {
+  queryClient.clear();
+});
+
+describe('Namespaces List', () => {
+  it('should render the Namespace Table with the columns', async () => {
+    const { getByText } = renderWithTheme(
+      <NamespaceList namespacesList={[]} />
+    );
+
+    getByText('Name');
+    getByText('Data Type');
+    getByText('Region');
+    getByText('Creation Date (UTC)');
+  });
+
+  it('should render the Namespaces data', async () => {
+    const namespaces = namespaceFactory.build();
+    const { getByText } = renderWithTheme(
+      <NamespaceList namespacesList={[namespaces]} />
+    );
+
+    getByText(namespaces.label);
+    getByText(namespaces.type);
+    getByText(namespaces.region);
+    getByText(formatDate(namespaces.created));
+  });
+});
